cf33e1090fe550611c4a4bb4c69a09ecce256201
Ross Alexandra <rossandus@hotmail.com>
2019-12-28 20:43:21 -0800

    Unwedded from expo

---
+++ react/FourInARow/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import NavBar from './components/NavBar';
+import AppScreen from './components/AppScreen';
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentScreen: AppScreen.GAMESCREEN,
+      lastScreenVars: null,
+    }
+  }
+
+  updateScreen = (newScreen, lastScreenVars=null) => {
+    this.setState({
+      currentScreen: newScreen,
+      lastScreenVars: lastScreenVars,
+    });
+  }
+
+  render() {
+
+    return (
+      <React.Fragment>
+        <NavBar changeScreen={ this.updateScreen } />
+        <AppScreen screen={ this.state.currentScreen } changeScreen={ this.updateScreen } lastScreenVars={ this.state.lastScreenVars }/>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ react/FourInARow/components/AppScreen.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import GameScreen from './Screens/GameScreen';
+import SettingsScreen from './Screens/SettingsScreen';
+
+export default class AppScreen extends Component {
+
+    static INVALIDATE = -1;
+    static GAMESCREEN = 0;
+    static SETTINGSSCREEN = 1;
+    static WINSCREEN = 2;
+
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            currentScreen: AppScreen.GAMESCREEN,
+            freshScreen: false,
+        }
+    }
+
+    static getDerivedStateFromProps(props, state) {
+
+        if (props.screen == AppScreen.INVALIDATE) {
+            props.changeScreen(AppScreen.GAMESCREEN);
+            return {
+                currentScreen: AppScreen.GameScreen,
+                freshScreen: true,
+            };
+        }
+
+        else if (props.screen != state.currentScreen) {
+            return {
+                currentScreen: props.screen,
+                freshScreen: true
+            }
+        }
+
+        return null;
+    }
+
+    unfreshScreen = () => {
+        this.setState({
+            freshScreen: false,
+        });
+    }
+
+    render() {
+        let currentScreen;
+
+        if (this.state.currentScreen == AppScreen.GAMESCREEN) {
+            currentScreen = <GameScreen changeScreen={ this.props.changeScreen } newGame={ this.state.freshScreen } unfreshScreen={ this.unfreshScreen }/>;
+        }
+        else if (this.state.currentScreen == AppScreen.SETTINGSSCREEN) {
+            currentScreen = <SettingsScreen changeScreen={ this.props.changeScreen }/>;
+        }
+        else {
+            currentScreen = <Text>Loading...</Text>;
+        }
+
+        return (
+                <React.Fragment>
+                    { currentScreen }
+                </React.Fragment>
+            );
+    }
+}
