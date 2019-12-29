a27da4359e2caafdd50802f7b28b77e589ba3d57
Ross Alexandra <rossandus@hotmail.com>
2019-12-28 21:09:27 -0800

    App is now in immersive sticky mode.

--- react/FourInARow/App.js
+++ react/FourInARow/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { StatusBar } from 'react-native';
 import NavBar from './components/NavBar';
 import AppScreen from './components/AppScreen';
 class App extends Component {
